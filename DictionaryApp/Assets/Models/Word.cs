3311f6093b30f7857af286b64c0c6411fccddd23
Administrator <tyrantqiao@icloud.com>
2018-03-05 12:31:59 +0800

    Add project files.

---
+++ DictionaryApp/Assets/Models/Word.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryApp.Assets.Models
+{
+    class Word
+    {
+        String Name { get; set; }
+        String Explantion { get; set; }
+        public Word(String name,String explantion)
+        {
+            this.Name = name;
+            this.Explantion = explantion;
+        }
+    }
+}
