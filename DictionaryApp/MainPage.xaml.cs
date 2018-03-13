a34b57ebfdb8dac75b78c8273295a4e5e0e0fe78
Administrator <tyrantqiao@icloud.com>
2018-03-13 22:34:35 +0800

    resources no success

--- DictionaryApp/MainPage.xaml.cs
+++ DictionaryApp/MainPage.xaml.cs
@@ -16,6 +16,8 @@ using DictionaryApp.Assets.Models;
 using System.Collections.ObjectModel;
 using DictionaryApp.Assets.Pages;
 using DictionaryApp.Models;
+using Windows.UI.Xaml.Automation;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,6 +29,7 @@ namespace DictionaryApp
     public sealed partial class MainPage : Page
     {
         public static MainPage Current=null;
+        private bool isPaddingAdded = false;
         private List<NavMenuItem> navlist = new List<NavMenuItem>(
             new[]
             {
@@ -53,41 +56,178 @@ namespace DictionaryApp
         public MainPage()
         {
             this.InitializeComponent();
-            Current = this;
+
+            this.Loaded += (sender, args) =>
+            {
+                Current = this;
+
+                this.CheckTogglePaneButtonSizeChanged();
+
+                var titleBar = Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar;
+                titleBar.IsVisibleChanged += TitleBar_IsVisibleChanged;
+            };
+
+            this.RootSplitView.RegisterPropertyChangedCallback(
+                SplitView.DisplayModeProperty,
+                (s, a) =>
+                {
+                    // Ensure that we update the reported size of the TogglePaneButton when the SplitView's
+                    // DisplayMode changes.
+                    this.CheckTogglePaneButtonSizeChanged();
+                });
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += SystemNavigationManager_BackRequested;
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            
+            NavMenuList.ItemsSource = navlist;
         }
 
-        private void HamburgerMenuButton_Click(object sender, RoutedEventArgs e)
+
+        private void SystemNavigationManager_BackRequested(object sender, BackRequestedEventArgs e)
         {
-            MenuSplitView.IsPaneOpen = !MenuSplitView.IsPaneOpen;
+            bool handled = e.Handled;
+            this.BackRequested(ref handled);
+            e.Handled = handled;
         }
 
-        
-
-        private void HomeBoxItem_Tapped(object sender, TappedRoutedEventArgs e)
+        private void BackRequested(ref bool handled)
         {
-            MenuSplitView.IsPaneOpen = !MenuSplitView.IsPaneOpen;
+            // Get a hold of the current frame so that we can inspect the app back stack.
+
+            if (this.AppFrame == null)
+                return;
+
+            // Check to see if this is the top-most page on the app back stack.
+            if (this.AppFrame.CanGoBack && !handled)
+            {
+                // If not, set the event to handled and go back to the previous page in the app.
+                handled = true;
+                this.AppFrame.GoBack();
+            }
         }
 
-        private void ReciteBoxItem_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            MenuSplitView.IsPaneOpen = !MenuSplitView.IsPaneOpen;
-        }
-
-        private void SettingBoxItem_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            MenuSplitView.IsPaneOpen = !MenuSplitView.IsPaneOpen;
-        }
 
         private void NavMenuItemContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
         {
-            String name;
-            //TODO ss
+            if (!args.InRecycleQueue && args.Item != null && args.Item is NavMenuItem)
+            {
+                args.ItemContainer.SetValue(AutomationProperties.NameProperty, ((NavMenuItem)args.Item).Label);
+            }
+            else
+            {
+                args.ItemContainer.ClearValue(AutomationProperties.NameProperty);
+            }
         }
 
-        private void NavMenuList_ItemInvoked(object sender, ListViewItem e)
+        public void OpenNavePane()
         {
-            //TODO ss
-            String name;
+            TogglePaneButton.IsChecked = true;
+            NavPaneDivider.Visibility = Visibility.Visible;
         }
+
+        private void TogglePaneButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.CheckTogglePaneButtonSizeChanged();
+        }
+
+        private void TogglePaneButton_Checked(object sender, RoutedEventArgs e)
+        {
+            NavPaneDivider.Visibility = Visibility.Visible;
+            this.CheckTogglePaneButtonSizeChanged();
+        }
+
+        private void CheckTogglePaneButtonSizeChanged()
+        {
+            if (this.RootSplitView.DisplayMode == SplitViewDisplayMode.Inline ||
+                this.RootSplitView.DisplayMode == SplitViewDisplayMode.Overlay)
+            {
+                var transform = this.TogglePaneButton.TransformToVisual(this);
+                var rect = transform.TransformBounds(new Rect(0, 0, this.TogglePaneButton.ActualWidth, this.TogglePaneButton.ActualHeight));
+                this.TogglePaneButtonRect = rect;
+            }
+            else
+            {
+                this.TogglePaneButtonRect = new Rect();
+            }
+
+            var handler = this.TogglePaneButtonRectChanged;
+            if (handler != null)
+            {
+                // handler(this, this.TogglePaneButtonRect);
+                handler.DynamicInvoke(this, this.TogglePaneButtonRect);
+            }
+        }
+
+        public Rect TogglePaneButtonRect
+        {
+            get;
+            private set;
+        }
+
+        public event TypedEventHandler<MainPage, Rect> TogglePaneButtonRectChanged;
+
+        private void AppShell_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            FocusNavigationDirection direction = FocusNavigationDirection.None;
+            switch (e.Key)
+            {
+                case Windows.System.VirtualKey.Left:
+                case Windows.System.VirtualKey.GamepadDPadLeft:
+                case Windows.System.VirtualKey.GamepadLeftThumbstickLeft:
+                case Windows.System.VirtualKey.NavigationLeft:
+                    direction = FocusNavigationDirection.Left;
+                    break;
+                case Windows.System.VirtualKey.Right:
+                case Windows.System.VirtualKey.GamepadDPadRight:
+                case Windows.System.VirtualKey.GamepadLeftThumbstickRight:
+                case Windows.System.VirtualKey.NavigationRight:
+                    direction = FocusNavigationDirection.Right;
+                    break;
+
+                case Windows.System.VirtualKey.Up:
+                case Windows.System.VirtualKey.GamepadDPadUp:
+                case Windows.System.VirtualKey.GamepadLeftThumbstickUp:
+                case Windows.System.VirtualKey.NavigationUp:
+                    direction = FocusNavigationDirection.Up;
+                    break;
+
+                case Windows.System.VirtualKey.Down:
+                case Windows.System.VirtualKey.GamepadDPadDown:
+                case Windows.System.VirtualKey.GamepadLeftThumbstickDown:
+                case Windows.System.VirtualKey.NavigationDown:
+                    direction = FocusNavigationDirection.Down;
+                    break;
+            }
+
+            if (direction != FocusNavigationDirection.None)
+            {
+                var control = FocusManager.FindNextFocusableElement(direction) as Control;
+                if (control != null)
+                {
+                    control.Focus(FocusState.Keyboard);
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void TitleBar_IsVisibleChanged(Windows.ApplicationModel.Core.CoreApplicationViewTitleBar sender, object args)
+        {
+            if (!this.isPaddingAdded && sender.IsVisible)
+            {
+                //add extra padding between window title bar and app content
+                double extraPadding = (Double)App.Current.Resources["DesktopWindowTopPadding"];
+                this.isPaddingAdded = true;
+
+                Thickness margin = NavMenuList.Margin;
+                NavMenuList.Margin = new Thickness(margin.Left, margin.Top + extraPadding, margin.Right, margin.Bottom);
+                margin = AppFrame.Margin;
+                AppFrame.Margin = new Thickness(margin.Left, margin.Top + extraPadding, margin.Right, margin.Bottom);
+                margin = TogglePaneButton.Margin;
+                TogglePaneButton.Margin = new Thickness(margin.Left, margin.Top + extraPadding, margin.Right, margin.Bottom);
+            }
+        }
+
+        public Frame AppFrame { get { return this.frame; } }
+        
     }
 }
---
+++ DictionaryApp/Triggers/IsTenFootTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.Xaml;
+
+namespace DictionaryApp.Triggers
+{
+    class IsTenFootTrigger : StateTriggerBase
+    {
+        private bool _isTenFootRequested;
+
+        public IsTenFootTrigger()
+        {
+            // Set default values.
+            IsTenFoot = true;
+        }
+
+        public bool IsTenFoot
+        {
+            get
+            {
+                return _isTenFootRequested;
+            }
+            set
+            {
+                _isTenFootRequested = value;
+                SetActive(SystemInformationHelpers.IsTenFootExperience == _isTenFootRequested);
+            }
+        }
+    }
+    public class SystemInformationHelpers
+    {
+        // Calculate this once and cache the result.
+        private static bool _isXbox = (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Xbox");
+
+        // For now, the 10-foot experience is enabled only on Xbox.
+        public static bool IsTenFootExperience => _isXbox;
+    }
+}
