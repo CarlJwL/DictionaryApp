7e9a901d3f660f0536a18d1cd0b56be49762aa19
Administrator <tyrantqiao@icloud.com>
2018-03-15 21:17:14 +0800

    Feel tired, changing to sql

---
+++ DictionaryApp/Models/Weather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace DictionaryApp.Models
+{
+    class Weather
+    {
+        public double Temperture { get; set; }
+        public Symbol Symbol { get; set; }
+        public char SymbolAsChar => (char)this.Symbol;
+        /// <summary>
+        /// <para>sun=0</para>
+        /// <para>Rain=1</para>
+        /// <para>Snow=2</para>
+        /// <para>Cloudy=3</para>
+        /// </summary>
+        public string WeatherType { get; set; }
+    }
+}
